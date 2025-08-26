366a62ea4a790cc25f7949247db91bb03e36a892
Kyle Matthew Poblete <kylematthewpobletena@gmail.com>
2025-08-26 22:54:49 +0800

    added separated components for sidebar

---
+++ resources/js/components/nav-main.tsx
@@ -0,0 +1,39 @@
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuItem,
+    SidebarMenuButton,
+} from "@/components/ui/sidebar"
+import { LucideIcon } from "lucide-react"
+
+export function NavMain({
+   items, 
+}: {
+    items: {
+        title: string
+        url: string
+        icon: LucideIcon
+    }[]
+}) {
+    return (
+        <SidebarGroup>
+          <SidebarGroupLabel>Menu</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
