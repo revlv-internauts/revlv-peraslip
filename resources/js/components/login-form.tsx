66d87f5f8e917523423a2430d703ec63783a1b60
Kyle Matthew Poblete <kylematthewpobletena@gmail.com>
2025-10-10 21:08:22 +0800

    added form method and action

--- resources/js/components/login-form.tsx
+++ resources/js/components/login-form.tsx
@@ -21,6 +21,8 @@ export function LoginForm({ className, ...props }: React.ComponentProps<"form">)
 
   return (
     <form 
+        method="POST"
+        action="/login"
         onSubmit={handleSubmit}
         className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
